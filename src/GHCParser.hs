bdacfd4b135bf6339f16fdfb1e984b022d5c706e
myuon <ioi.joi.koi.loi@gmail.com>
2019-03-06 23:40:39 +0900

    refactor: GHCParser

---
+++ src/GHCParser.hs
@@ -0,0 +1,29 @@
+module GHCParser(
+  getDynFlags,
+  runParser,
+  parseModuleFile
+) where
+
+import qualified Lexer
+import qualified Parser
+import qualified StringBuffer
+import qualified SrcLoc
+import qualified FastString
+import qualified Outputable
+import qualified GHC
+import qualified GHC.Paths
+
+getDynFlags :: IO (GHC.DynFlags)
+getDynFlags = GHC.runGhc (Just GHC.Paths.libdir) GHC.getSessionDynFlags
+
+runParser :: GHC.DynFlags -> FilePath -> Lexer.P a -> IO (Lexer.ParseResult a)
+runParser flags path parser = do
+  file <- StringBuffer.hGetStringBuffer path
+
+  let pstate = Lexer.mkPState flags file (SrcLoc.mkRealSrcLoc (FastString.fsLit "") 0 0)
+  return $ Lexer.unP parser pstate
+
+parseModuleFile :: FilePath -> IO (Lexer.ParseResult (GHC.Located (GHC.HsModule GHC.GhcPs)))
+parseModuleFile path = do
+  flags <- getDynFlags
+  runParser flags path Parser.parseModule
--- src/Main.hs
+++ src/Main.hs
@@ -9,21 +9,15 @@ import qualified FastString
 import qualified Outputable
 import qualified GHC
 import qualified GHC.Paths
+import qualified GHCParser
 
 filename :: FilePath
 filename = "./src/Main.hs"
 
-runParser :: GHC.DynFlags -> FilePath -> Lexer.P a -> IO (Lexer.ParseResult a)
-runParser flags path parser = do
-  file <- StringBuffer.hGetStringBuffer path
-
-  let pstate = Lexer.mkPState flags file (SrcLoc.mkRealSrcLoc (FastString.fsLit "") 0 0)
-  return $ Lexer.unP parser pstate
-
 main :: IO ()
 main = do
-  flags <- GHC.runGhc (Just GHC.Paths.libdir) GHC.getSessionDynFlags
-  r <- runParser flags filename Parser.parseModule
+  flags <- GHCParser.getDynFlags
+  r <- GHCParser.parseModuleFile filename
   case r of
     Lexer.POk _ h -> putStrLn $ Outputable.showSDoc flags $ Outputable.ppr h
     _ -> error "failed"
