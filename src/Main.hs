321e236d590f64353c1820f942abadf144cb5a41
myuon <ioi.joi.koi.loi@gmail.com>
2019-03-07 20:37:09 +0900

    feat: loadAST, applyRules, recoverSrcLoc

--- src/Main.hs
+++ src/Main.hs
@@ -2,16 +2,29 @@
 module Main where
 
 import qualified Lexer
+import qualified SrcLoc
 import qualified Outputable
+import qualified HsSyn
 import qualified Hayway.Parser
 
 filename :: FilePath
 filename = "./src/Main.hs"
 
+loadAST :: FilePath -> IO (Either Outputable.SDoc (SrcLoc.Located (HsSyn.HsModule HsSyn.GhcPs)))
+loadAST path = do
+  r <- Hayway.Parser.parseModuleFile filename
+  return $ case r of
+    Lexer.POk _ h -> Right h
+    Lexer.PFailed _ _ d -> Left d
+
+applyRules = undefined
+
+recoverSrcLoc = undefined
+
 main :: IO ()
 main = do
   flags <- Hayway.Parser.getDynFlags
-  r <- Hayway.Parser.parseModuleFile filename
-  case r of
-    Lexer.POk _ h -> putStrLn $ Outputable.showSDoc flags $ Outputable.ppr h
-    _ -> error "failed"
+
+  loadAST filename >>= \case
+    Left d -> print $ Outputable.showSDoc flags d
+    Right r -> return ()
